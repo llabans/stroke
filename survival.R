5d37db2e03880bfd4ec68000a0c210be18893371
character <luis.labans@gmail.com>
2020-10-13 11:19:26 -0500

    survival analysis

--- survival.R
+++ survival.R
@@ -19,10 +19,13 @@ library(lubridate)
 
 setwd("~/Documentos/R/Stroke/death/")
 y2016deaths <- read.csv("yearsdeaths.cvs")
-levels(y2016deaths$cid10) #4
+levels(as.factor(y2016deaths$cid10)) #4
 str(y2016deaths)
 table(y2016deaths$cid10,y2016deaths$agecat,y2016deaths$condicion)
-
+y2016deaths$edad <- as.numeric(y2016deaths$edad)
+summarise(y2016deaths$edad)
+tapply(edad,condicion,sd)
+sd(edad)
 
 
 y2016deaths$agecat <- ifelse(y2016deaths$edad<55, 0, ifelse(y2016deaths$edad>=55 & y2016deaths$edad<75, 1, ifelse(y2016deaths$edad>=75, 2, NA))) %>%  
@@ -56,20 +59,44 @@ prop.res.cox1 #VIOLATION OF THE ASSUMPTION
 prop.res.cox2 <- cox.zph(coxph(survivalobj0 ~ exposure+sexo+agecat+strata(regions, level),data=y2016deaths))
 prop.res.cox2 #VIOLATION OF THE ASSUMPTION 
 
-##COX MODELS
-###### Weibull model####
-#######################eha
-install.packages("eha")
+########################
+###### WEIBULL MODEL####
+########################
 library(eha)
-ehawb1 <- aftreg(survivalobj0 ~ exposure+strata(regions, level),dist = "weibull",
-                 data = y2016deaths)
-ehawb1
-exp(confint(ehawb1))
 
-ehawb2 <- aftreg(survivalobj0 ~ exposure+agecat+sexo+strata(regions,level), dist="weibull",
+#Model not adjusted
+wb1 <- weibreg(survivalobj0 ~ exposure+strata(regions, level):cid10,
+               data = y2016deaths) #strata
+wb1
+#I60
+#exp(0.850 + 1.96*0.125)
+#exp(0.850 -1.96*0.125)
+#I61
+#exp(0.636 +1.96*0.117)
+#exp(0.636 -1.96*0.117)
+#I64
+#exp(0.393 +1.96*0.113)
+#exp(0.393 -1.96*0.113)
+#y2016deaths$sexo <- as.numeric(y2016deaths$sexo)
+#numeric covariables (edad, sex)
+
+#Adjusted model
+ehawb2 <- weibreg(survivalobj0 ~ exposure+sexo+edad+strata(regions,level),
                   data = y2016deaths) #ajusted~strata
 ehawb2
-exp(confint(ehawb2))
+
+#I60
+exp(0.895 + 1.96*0.127)
+exp(0.895 -1.96*0.127)
+
+#I61
+exp(0.670 +1.96*0.117)
+exp(0.670 -1.96*0.117)
+
+#I64
+exp(0.374 +1.96*0.113)
+exp(0.374 -1.96*0.113)
+
 
 #################################################################################
 #Creating survival plot
@@ -87,17 +114,35 @@ losfit <- survfit(Surv(y2016deaths$los, y2016deaths$condicion=="Death") ~ y2016d
 
 palette()               # obtain the current palette
 
-(palette(gray(seq(0,.9,len = 25)))) # gray scales; print old palette
-matplot(outer(1:100, 1:30), type = "l", lty = 1,lwd = 2, col = 1:30,
-        main = "Gray Scales Palette",
-        sub = "palette(gray(seq(0, .9, len=25)))")
-palette("default")      # reset back to the default
+#(palette(gray(seq(0,.9,len = 25)))) # gray scales; print old palette
+#matplot(outer(1:100, 1:30), type = "l", lty = 1,lwd = 2, col = 1:30,
+#        main = "Gray Scales Palette",
+#        sub = "palette(gray(seq(0, .9, len=25)))")
+#palette("default")      # reset back to the default
 
 # Visualize with survminer
-ggsurvplot(losfit, data = y2016deaths, main = "Survival curve",
-           submain = "Based on Kaplan-Meier estimates", legend = c(0.1, 0.2),
-           legend.title = "Strata",  risk.table = TRUE, xlim = c(0,30),
-           break.time.by = 5, risk.table.y.text.col = T, risk.table.y.text = FALSE, conf.int = TRUE,conf.int.style=c("ribbon"), pval = TRUE, palette = gray(0:4/4))
+ggsurvplot(losfit, data = y2016deaths, main = "Survival curve", pval = TRUE, size = 0.5, pval.coord =c(2,0),
+           submain = "Based on Kaplan-Meier estimates", legend = c(0.2, 0.2),
+           font.submain = 18,
+           #font.x =  16,
+           #font.y = 16,
+           #font.tickslab = 16,
+           font.legend = 13,
+           #font.caption = 16,
+           #risk.table.fontsize = 6,
+           #risk.table.height = 0.45,
+           #font.family = 14,
+           #fontsize =6,
+           risk.table = TRUE, xlim = c(0,30), ylim =c(0.6,1),xlab = "Time in days",
+           legend.labs = c("Cerebral Infarction (I63) (n=122)","Intra-cerebral Hemorrhage (I61) (n=193)","Stroke not specified as hemorrhage or infarction (I64) (n=297)","Subarachnoid Hemorrhage (I60) (n=145)"), 
+           break.time.by = 5, risk.table.y.text.col = T, risk.table.y.text = FALSE, conf.int = TRUE,conf.int.style=c("ribbon"), ggtheme = theme_bw())
 
 
+#comment
 
+library(survival)
+library(MASS)
+data(gehan)
+gehansurv=Surv(gehan$time, gehan$cens)
+plot(survfit(gehansurv ~ gehan$treat), col=c("black", "red"), fun="cloglog")
+ggcoxzph(test.ph1)
